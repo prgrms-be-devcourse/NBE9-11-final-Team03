ac3a89594de73e72d4de072e4847e910c95d3270
parkJJae <wgpark0830@naver.com>
2026-06-17 10:43:23 +0900

    fix: 재능 수정 시 본문 내용 글자 수 제한 추가 및 엔í 및 오타수정

--- src/main/java/com/back/baton/domain/talent/dto/request/TalentUpdateReq.java
+++ src/main/java/com/back/baton/domain/talent/dto/request/TalentUpdateReq.java
@@ -11,7 +11,7 @@ public record TalentUpdateReq(
         String title,
 
         @NotBlank(message = "내용은 필수입니다.")
-        @Size(max = 10000, message = "제목은 10000자 이하여야 합니다.")
+        @Size(max = 10000, message = "내용은 10000자 이하여야 합니다.")
         String content,
 
         @NotNull @Positive(message = "예상 소요 시간은 1 이상이어야 합니다.")
