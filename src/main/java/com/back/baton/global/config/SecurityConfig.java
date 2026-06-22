5a5e25f598926f7307cb186a89413eab1bd66101
YJin33 <ujin3261@gmail.com>
2026-06-22 17:41:45 +0900

    BATON-107 feat: SecurityConfig cors 및 authorize 수정

--- src/main/java/com/back/baton/global/config/SecurityConfig.java
+++ src/main/java/com/back/baton/global/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.back.baton.global.config;
 
-import com.back.baton.global.security.JwtTokenProvider;
 import com.back.baton.global.filter.JwtAuthenticationFilter;
+import com.back.baton.global.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +11,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -29,8 +34,9 @@ public class SecurityConfig {
                 .cors(withDefaults())
                 .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/api/v1/auth/signup", "/api/v1/auth/login", "/api/v1/auth/reissue").permitAll()
+                    .requestMatchers("/swagger-ui/**","/swagger-ui.html","/v3/api-docs/**").permitAll() // swagger 활성화
 //                    .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
-                    .anyRequest().permitAll()
+                    .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         ;
@@ -38,4 +44,18 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 허용할 도메인 및 메서드 설정
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://54.116.23.255"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        configuration.setAllowCredentials(true); // 인증 정보 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
