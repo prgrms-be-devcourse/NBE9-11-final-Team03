ebbc002d58c09c8e4bdd9e7720b1f303912c290d
YJin33 <ujin3261@gmail.com>
2026-06-24 11:24:17 +0900

    BATON-93 chore: swagger 설정 추가

--- src/main/java/com/back/baton/domain/profile/controller/ProfileController.java
+++ src/main/java/com/back/baton/domain/profile/controller/ProfileController.java
@@ -8,6 +8,8 @@ import com.back.baton.global.response.ApiResponses;
 import com.back.baton.global.response.code.SuccessCode;
 import com.back.baton.global.security.CurrentUser;
 import com.back.baton.global.security.SecurityUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +18,15 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/profiles")
+@Tag(name = "Profile", description = "프로필 생성/수정 및 조회 API")
 public class ProfileController {
     private final ProfileService profileService;
 
     @PatchMapping
+    @Operation(
+            summary = "프로필 수정",
+            description = "현재 로그인한 작성자가 프로필의 한줄소개, 프로필이미지, 가진/원하는 재능 카테고리, 포트폴리오 링크를 원하는것만 골라 수정합니다."
+    )
     public ResponseEntity<ApiResponse<ProfileUpdateRes>> updateProfile(
             @RequestBody @Valid ProfileUpdateReq req,
             @CurrentUser SecurityUser currentUser
