341986c8901897779802a9150a64ca35d29fb8b6
jinwoo <realize2578@gmail.com>
2026-06-25 17:11:31 +0900

    BATON-138 fix: Swagger JWT 인증 설정 추가

---
+++ src/main/java/com/back/baton/global/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.back.baton.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String JWT_SECURITY_SCHEME = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(JWT_SECURITY_SCHEME, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(JWT_SECURITY_SCHEME));
+    }
+}
